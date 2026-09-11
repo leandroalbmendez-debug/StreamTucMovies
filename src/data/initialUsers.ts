f8006d0b7d66f87242847321a9521c61002efe1d
Lucas Lencina <lucasfacdef@gmail.com>
2026-09-11 00:05:27 -0300

    feat: add initial users

---
+++ src/data/initialUsers.ts
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+import type { User } from "../types/User";
+
+export const initialUsers: User[] = [
+  {
+    id: uuidv4(),
+    email: "admin@streamtuc.com",
+    password: "Admin123",
+    username: "Administrator",
+    role: "admin",
+    plan: "premium",
+    favorites: [],
+    comments: [],
+  },
+  {
+    id: uuidv4(),
+    email: "juan@gmail.com",
+    password: "Juan123",
+    username: "Juan Perez",
+    role: "client",
+    plan: "free",
+    favorites: [1, 3],
+    comments: [
+      {
+        page: "Series",
+        text: "Me gusta mucho esta serie",
+      },
+    ],
+  },
+];
