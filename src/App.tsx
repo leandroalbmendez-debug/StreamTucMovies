e8be0beb02a42294cf21895a1ee5a2d0aa1bfde8
Lucas Lencina <lucasfacdef@gmail.com>
2026-09-17 12:35:10 -0300

    " Se agrego ruta para buscador"

--- src/App.tsx
+++ src/App.tsx
@@ -13,95 +13,93 @@ import type { User } from "./types/User";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import "./css/App.css";
 import Layout from "./pages/Layout";
+import { Search } from "./pages/Search";
 
 function ProtectedAdmin() {
-	const [loggedUser] = useLocalStorage<User | null>(
-		"streamtuc-logged-user",
-		null,
-	);
+  const [loggedUser] = useLocalStorage<User | null>(
+    "streamtuc-logged-user",
+    null,
+  );
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	if (!loggedUser) {
-		return (
-			<Navigate
-				to="/login"
-				replace
-			/>
-		);
-	}
+  if (!loggedUser) {
+    return <Navigate to="/login" replace />;
+  }
 
-	if (loggedUser.role !== "admin") {
-		const handleBackToLogin = () => {
-			navigate("/login");
-		};
+  if (loggedUser.role !== "admin") {
+    const handleBackToLogin = () => {
+      navigate("/login");
+    };
 
-		return (
-			<Container className="py-5 text-center">
-				<h1>No tenés permisos</h1>
+    return (
+      <Container className="py-5 text-center">
+        <h1>No tenés permisos</h1>
 
-				<p className="mb-4">
-					Esta sección es exclusiva para administradores de STREAMTUC.
-				</p>
+        <p className="mb-4">
+          Esta sección es exclusiva para administradores de STREAMTUC.
+        </p>
 
-				<Button
-					variant="primary"
-					onClick={handleBackToLogin}>
-					Volver al inicio de sesión
-				</Button>
-			</Container>
-		);
-	}
+        <Button variant="primary" onClick={handleBackToLogin}>
+          Volver al inicio de sesión
+        </Button>
+      </Container>
+    );
+  }
 
-	return <Admin />;
+  return <Admin />;
 }
 
 const router = createBrowserRouter([
-	{
-		element: <Layout />,
-		children: [
-			{
-				path: "/",
-				element: <Index />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/register",
-				element: <Register />,
-			},
-			{
-				path: "/admin",
-				element: <ProtectedAdmin />,
-			},
-			{
-				path: "/catalog",
-				element: <Catalog />,
-			},
-			{
-				path: "/favorites",
-				element: <Favorites />,
-			},
-			{
-				path: "/detail",
-				element: <Detail />,
-			},
-			{
-				path: "/detail/:movieId",
-				element: <Detail />,
-			},
-			{
-				path: "*",
-				element: <NotFound />,
-			},
-		],
-	},
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Index />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/admin",
+        element: <ProtectedAdmin />,
+      },
+      {
+        path: "/catalog",
+        element: <Catalog />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+      {
+        path: "/detail",
+        element: <Detail />,
+      },
+      {
+        path: "/detail/:movieId",
+        element: <Detail />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
